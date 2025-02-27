5adb602c392e89d7d1ebc1b0502e08759b916ff6
Sourav Shib <118858686+margr158@users.noreply.github.com>
2025-02-27 04:52:49 -0500

    Update v5_main.py

--- RPI_Code/v5_main.py
+++ RPI_Code/v5_main.py
@@ -1,34 +1,63 @@
-import time
 import firebase_admin
 from firebase_admin import credentials, db
+import time
 
-# Initialize Firebase
-firebase_key_path = "/home/pi/project_code/firebase_key.json"  # Path to your Firebase credentials JSON
-cred = credentials.Certificate(firebase_key_path)
+# Initialize Firebase (Ensure you have a Firebase Admin SDK JSON file or use anonymous authentication)
+cred = credentials.Certificate("/home/pi/project_code/firebase_key.json")  # Replace with your actual credentials file
 firebase_admin.initialize_app(cred, {
-    "databaseURL": "https://simple-sprouts-database-default-rtdb.firebaseio.com/"
+    'databaseURL': 'https://simple-sprouts-database-default-rtdb.firebaseio.com/'
 })
 
-# Firebase Database References
-levels_ref = db.reference("/levels")
-general_info_ref = db.reference("/general_info")
+# Reference to the database location
+ref = db.reference("flags_test")
 
-print("Reading Firebase data every 2 seconds...")
+def fetch_flags():
+    data = ref.get()
+    if data:
+        app_open = data.get("app_open", False)
+        level_under_test = data.get("level_under_test", 0)
+        bottom_initialized = data.get("bottom_initialized", False)
+        bottom_mode = data.get("bottom_mode", "manual")
+        bottom_man_light = data.get("bottom_man_light", False)
+        bottom_man_water = data.get("bottom_man_water", False)
+        botton_mode_edited = data.get("botton_mode_edited", False)
+        bottom_light_ref_off_hrs = data.get("bottom_light_ref_off_hrs", 0)
+        bottom_light_ref_on_hrs = data.get("bottom_light_ref_on_hrs", 0)
+        bottom_light_ref_off_mins = data.get("bottom_light_ref_off_mins", 0)
+        bottom_light_ref_on_mins = data.get("bottom_light_ref_on_mins", 0)
+        bottom_light_tts_hrs = data.get("bottom_light_tts_hrs", 0)
+        bottom_light_tts_mins = data.get("bottom_light_tts_mins", 0)
+        bottom_water_ref_days = data.get("bottom_water_ref_days", 0)
+        bottom_water_ref_hrs = data.get("bottom_water_ref_hrs", 0)
+        bottom_water_ref_min = data.get("bottom_water_ref_min", 0)
+        bottom_water_ttw_days = data.get("bottom_water_ttw_days", 0)
+        bottom_water_ttw_hrs = data.get("bottom_water_ttw_hrs", 0)
+        bottom_water_ttw_min = data.get("bottom_water_ttw_min", 0)
+        top_initialized = data.get("top_initialized", False)
+        top_mode = data.get("top_mode", "manual")
+        top_man_light = data.get("top_man_light", False)
+        top_man_water = data.get("top_man_water", False)
+        top_mode_edited = data.get("top_mode_edited", False)
+        top_light_ref_off_hrs = data.get("top_light_ref_off_hrs", 0)
+        top_light_ref_on_hrs = data.get("top_light_ref_on_hrs", 0)
+        top_light_ref_off_mins = data.get("top_light_ref_off_mins", 0)
+        top_light_ref_on_mins = data.get("top_light_ref_on_mins", 0)
+        top_light_tts_hrs = data.get("top_light_tts_hrs", 0)
+        top_light_tts_mins = data.get("top_light_tts_mins", 0)
+        top_water_ref_days = data.get("top_water_ref_days", 0)
+        top_water_ref_hrs = data.get("top_water_ref_hrs", 0)
+        top_water_ref_min = data.get("top_water_ref_min", 0)
+        top_water_ttw_days = data.get("top_water_ttw_days", 0)
+        top_water_ttw_hrs = data.get("top_water_ttw_hrs", 0)
+        top_water_ttw_min = data.get("top_water_ttw_min", 0)
+        
+        print("Updated Flags:")
+        for key, value in data.items():
+            print(f"{key}: {value}")
+    else:
+        print("No data found in flags_test")
 
-try:
-    while True:
-        # Fetch data from Firebase
-        levels_data = levels_ref.get()
-        general_info_data = general_info_ref.get()
-
-        # Default to empty dictionary if no data is found
-        levels_data = levels_data if levels_data else {}
-        general_info_data = general_info_data if general_info_data else {}
-
-        print("Levels Data:", levels_data)
-        print("General Info Data:", general_info_data)
-
-        time.sleep(3)  # Wait for 2 seconds before the next read
-
-except KeyboardInterrupt:
-    print("\nScript interrupted by user. Exiting...")
+# Continuously fetch data every 3 seconds
+while True:
+    fetch_flags()
+    time.sleep(3)
