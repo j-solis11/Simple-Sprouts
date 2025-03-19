a69fd08c7547adcd531b4b831311618addd28f04
Sourav Shib <118858686+margr158@users.noreply.github.com>
2025-03-19 17:59:39 -0400

    Update nv1.py

--- RPI_Code/nv1.py
+++ RPI_Code/nv1.py
@@ -1,7 +1,7 @@
 import time
 import gpiod
 import time
-#import threading
+import threading
 import firebase_admin
 from firebase_admin import credentials, db
 
@@ -38,12 +38,15 @@ firebase_admin.initialize_app(cred, {
 # Reference to the database location
 ref = db.reference("flags_test")
 
+count_top_light = 0
+count_bottom_light = 0
+
 def manual_mode():
 
     data = ref.get()
 
     level_under_test = data.get("level_under_test", 1)
-    print(level_under_test)
+    #print(level_under_test)
 
     if level_under_test == 1:
         bottom_initialized = data.get("bottom_initialized", False)
@@ -99,8 +102,107 @@ def manual_mode():
                     #led_line_9.set_value(0)
                     print("LED: 5 - OFF")
     
+'''
+def scheduling_mode():
+    target_top_light = 0
+    target_bottom_light = 0
+    target_top_water = 0
+    target_bottom_water = 0
+    total_off = 0
+    total_on = 0
+
+    curr_counter = global_counter
+
+    data = ref.get()
+
+    bottom_initialized = data.get("bottom_initialized", False)
+    top_initialized = data.get("top_initialized", False)
+
+    if bottom_initialized == True:
+        bottom_mode = data.get("bottom_mode", "manual")
+
+        if bottom_mode == "scheduling":
+            bottom_mode_light_edit = data.get("bottom_mode_light_edit", False)
+            bottom_mode_water_edit = data.get("bottom_mode_water_edit", False)
+
+            if bottom_mode_light_edit == True:
+                bottom_light_ref_off_hrs = data.get("bottom_light_ref_off_hrs", 0)
+                bottom_light_ref_on_hrs = data.get("bottom_light_ref_on_hrs", 0)
+                bottom_light_ref_off_mins = data.get("bottom_light_ref_off_mins", 0)
+                bottom_light_ref_on_mins = data.get("bottom_light_ref_on_mins", 0)
+
+                total_on = (bottom_light_ref_on_hrs*1) + (bottom_light_ref_on_mins*1)
+                total_off = (bottom_light_ref_off_hrs*1) + (bottom_light_ref_off_mins*1)
+
+                target_top_light = curr_counter + total_on
+                #add other counters
+                ref.update({"bottom_mode_light_edit": False})
+
+            if curr_counter < target_top_light:
+                print("top light on")
+            
+            if curr_counter
+
+'''
+
+def top_light():
+    target_top_light = 0
+    total_off = 0
+    total_on = 0
+    light_on = False
+
+    data = ref.get()
+
+    top_initialized = data.get("top_initialized", False)
+
+    if top_initialized == True:
+        top_mode = data.get("top_mode", "manual")
+        
+        if top_mode == "scheduling":
+            top_mode_light_edit = data.get("top_mode_light_edit", False)
+
+            if top_mode_light_edit == True:
+                top_light_ref_off_hrs = data.get("top_light_ref_off_hrs", 0)
+                top_light_ref_on_hrs = data.get("top_light_ref_on_hrs", 0)
+                top_light_ref_off_mins = data.get("top_light_ref_off_mins", 0)
+                top_light_ref_on_mins = data.get("top_light_ref_on_mins", 0)
+
+                total_on = (top_light_ref_on_hrs*1) + (top_light_ref_on_mins*1)
+                total_off = (top_light_ref_off_hrs*1) + (top_light_ref_off_mins*1)
+
+                target_top_light = count_top_light + total_on
+                ref.update({"top_mode_light_edit": False})
+                light_on = True
+
+            if (count_top_light < target_top_light) and light_on == True:
+                print("top light on")
+
+            if (count_top_light > target_top_light) and light_on == True:
+                print("top light off")
+                light_on == False
+                target_top_light = count_top_light + total_off
+            
+            if (count_top_light < target_top_light) and light_on == False:
+                print("top light off")
+            
+            if (count_top_light > target_top_light) and light_on == False:
+                print("top light on")
+                light_on = True
+                target_top_light = count_top_light + total_on
+
+'''
+def bottom_light():
     time.sleep(1)
+'''
+
+def timer():
+    time.sleep(5)
+    count_bottom_light = count_bottom_light + 5
+    count_top_light = count_top_light + 5
+
+timer_thread = threading.Thread(target=timer, daemon=True)
+timer_thread.start()
 
 while True:
+    top_light()
     manual_mode()
-    
